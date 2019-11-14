0448fb3a2744c517f392f65a2ac51ff27c818b3c
Carlos Barreto <ca.barreto@uniandes.edu.co>
2019-11-14 12:11:58 -0500

    Solucion runge kuta friccion

---
+++ makefile
@@ -0,0 +1,8 @@
+rk4.png: rk4.dat rk4.py
+	python rk4.py
+
+%.dat : a.out
+	./a.out
+
+a.out: rk4.cpp
+	c++ rk4.cpp
---
+++ rk4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void rk4();
+
+int main() {
+	cout << "hola";
+	return 0;
+}
+
+void r4k() {
+
+}
